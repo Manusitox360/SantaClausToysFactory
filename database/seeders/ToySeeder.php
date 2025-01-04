599a25589e4167c2e44978b2be8f8aa401118123
Antonio Guillén García <119064532+AntonioGuillen123@users.noreply.github.com>
2025-01-04 03:38:42 +0100

    feat: Add type id field on toy seeder and change things

--- database/seeders/ToySeeder.php
+++ database/seeders/ToySeeder.php
@@ -16,134 +16,155 @@ class ToySeeder extends Seeder
         $toys = [
                 // Toys for ages 0 and up
                 [
-                    'name' => "Rattle",
+                    'name' => "Colorful Baby Rattle",
                     'description' => "Colorful rattle that stimulates the baby's senses.",
                     'image' => "https://res.cloudinary.com/dq2kswexq/image/upload/v1734336018/ChristmasToyFactory/Rattle.jpg",
-                    'minimum_age_id' => 1
+                    'minimum_age_id' => 1,
+                    'type_id' => 1
                 ],
                 [
-                    'name' => "Soft Plush",
+                    'name' => "Snuggly Bear Plush Toy",
                     'description' => "Soft plush toy with textures and contrasting colors.",
                     'image' => "https://res.cloudinary.com/dq2kswexq/image/upload/v1734336150/ChristmasToyFactory/SoftPlush.jpg",
-                    'minimum_age_id' => 1
+                    'minimum_age_id' => 1,
+                    'type_id' => 2
                 ],
                 [
-                    'name' => "Crib Mobile",
+                    'name' => " Musical Crib Mobile with Rotating Figures",
                     'description' => "Musical mobile with rotating figures for the crib.",
                     'image' => "https://res.cloudinary.com/dq2kswexq/image/upload/v1734336150/ChristmasToyFactory/CribMobile.jpg",
-                    'minimum_age_id' => 1
+                    'minimum_age_id' => 1,
+                    'type_id' => 1
                 ],
                 [
-                    'name' => "Plastic Books",
+                    'name' => "Baby Books Set",
                     'description' => "Books for safe handling and exploration.",
                     'image' => "https://res.cloudinary.com/dq2kswexq/image/upload/v1734336150/ChristmasToyFactory/PlasticBooks.jpg",
-                    'minimum_age_id' => 1
+                    'minimum_age_id' => 1,
+                    'type_id' => 3
                 ],
                 [
                     'name' => "Large Stackable Blocks",
                     'description' => "Soft blocks to encourage motor skills.",
                     'image' => "https://res.cloudinary.com/dq2kswexq/image/upload/v1734336150/ChristmasToyFactory/LargeStackableBlocks.jpg",
-                    'minimum_age_id' => 1
+                    'minimum_age_id' => 1,
+                    'type_id' => 4
                 ],
                 [
                     'name' => "Interactive Play Mats",
                     'description' => "Mats with lights, sounds, and varied textures.",
                     'image' => "https://res.cloudinary.com/dq2kswexq/image/upload/v1734336150/ChristmasToyFactory/InteractivePlayMats.jpg",
-                    'minimum_age_id' => 1
+                    'minimum_age_id' => 1,
+                    'type_id' => 5
                 ],
                 [
-                    'name' => "Teething Toy",
+                    'name' => "Silicone Teething Ring for Babies",
                     'description' => "Soft and safe teething toy to soothe gums.",
                     'image' => "https://res.cloudinary.com/dq2kswexq/image/upload/v1734336150/ChristmasToyFactory/TeethingToy.jpg",
-                    'minimum_age_id' => 1
+                    'minimum_age_id' => 1,
+                    'type_id' => 6
                 ],
 
                 // Toys for ages 7 and up
                 [
-                    'name' => "Advanced LEGO",
+                    'name' => "Advanced LEGO Star Wars Ship",
                     'description' => "LEGO set with challenging models.",
                     'image' => "https://res.cloudinary.com/dq2kswexq/image/upload/v1734336150/ChristmasToyFactory/AdvancedLego.jpg",
-                    'minimum_age_id' => 2
+                    'minimum_age_id' => 2,
+                    'type_id' => 7
                 ],
                 [
-                    'name' => "Educational Board Game",
+                    'name' => "Educational Go Genius in English",
                     'description' => "Monopoly Junior for learning through play.",
                     'image' => "https://res.cloudinary.com/dq2kswexq/image/upload/v1734336150/ChristmasToyFactory/EducationalBoardGame.jpg",
-                    'minimum_age_id' => 2
+                    'minimum_age_id' => 2,
+                    'type_id' => 8
                 ],
                 [
-                    'name' => "Experiment Kit",
+                    'name' => "Idler Gear Experiment Kit",
                     'description' => "Kit for exploring science with fun experiments.",
                     'image' => "https://res.cloudinary.com/dq2kswexq/image/upload/v1734336150/ChristmasToyFactory/ExperimentKit.jpg",
-                    'minimum_age_id' => 2
+                    'minimum_age_id' => 2,
+                    'type_id' => 9
                 ],
                 [
-                    'name' => "100-300 Piece Puzzles",
+                    'name' => "Ocean Adventure Puzzle (300 pieces)",
                     'description' => "Puzzles that stimulate logic and concentration.",
                     'image' => "https://res.cloudinary.com/dq2kswexq/image/upload/v1734336150/ChristmasToyFactory/Puzzles.jpg",
-                    'minimum_age_id' => 2
+                    'minimum_age_id' => 2,
+                    'type_id' => 10
                 ],
                 [
-                    'name' => "Drawing and Craft Materials",
+                    'name' => "Creative Art Set: Watercolors, Clay & More",
                     'description' => "Kits with watercolors, clay, and creative crafts.",
                     'image' => "https://res.cloudinary.com/dq2kswexq/image/upload/v1734336150/ChristmasToyFactory/CraftMaterials.jpg",
-                    'minimum_age_id' => 2
+                    'minimum_age_id' => 2,
+                    'type_id' => 11
                 ],
                 [
-                    'name' => "Bicycle",
+                    'name' => "Kids' Blue Mountain Bike (20-inch wheels)",
                     'description' => "Vehicle to develop balance and physical activity.",
                     'image' => "https://res.cloudinary.com/dq2kswexq/image/upload/v1734336150/ChristmasToyFactory/Bicycle.jpg",
-                    'minimum_age_id' => 2
+                    'minimum_age_id' => 2,
+                    'type_id' => 12
                 ],
                 [
-                    'name' => "Basic Tech Toys",
+                    'name' => "Mini Programmable Robot for Kids",
                     'description' => "Simple programmable robots or small drones.",
                     'image' => "https://res.cloudinary.com/dq2kswexq/image/upload/v1734336150/ChristmasToyFactory/BasicTechToys.jpg",
-                    'minimum_age_id' => 2
+                    'minimum_age_id' => 2,
+                    'type_id' => 13
                 ],
 
                 // Toys for ages 12 and up
                 [
-                    'name' => "Camera Drone",
+                    'name' => "High-Tech Drone with HD Camera",
                     'description' => "Advanced drone with remote control and integrated camera.",
                     'image' => "https://res.cloudinary.com/dq2kswexq/image/upload/v1734336150/ChristmasToyFactory/CameraDrone.jpg",
-                    'minimum_age_id' => 3
+                    'minimum_age_id' => 3,
+                    'type_id' => 13
                 ],
                 [
-                    'name' => "Strategy Game",
-                    'description' => "Board game like Catan to develop strategic skills.",
+                    'name' => "Risk: The Strategy Board Game",
+                    'description' => "Board game like Risk to develop strategic skills.",
                     'image' => "https://res.cloudinary.com/dq2kswexq/image/upload/v1734336150/ChristmasToyFactory/StrategyGame.jpg",
-                    'minimum_age_id' => 3
+                    'minimum_age_id' => 3,
+                    'type_id' => 8
                 ],
                 [
-                    'name' => "Robotics Kit",
+                    'name' => "Arduino Starter Kit for Robotics Projects",
                     'description' => "Arduino kit to learn robotics and programming.",
                     'image' => "https://res.cloudinary.com/dq2kswexq/image/upload/v1734336150/ChristmasToyFactory/RoboticsKit.jpg",
-                    'minimum_age_id' => 3
+                    'minimum_age_id' => 3,
+                    'type_id' => 14
                 ],
                 [
-                    'name' => "Video Games",
+                    'name' => "The Legend of Yuan: The crazy man on the road",
                     'description' => "Adventure or strategy titles suitable for teenagers.",
                     'image' => "https://res.cloudinary.com/dq2kswexq/image/upload/v1734336150/ChristmasToyFactory/VideoGames.jpg",
-                    'minimum_age_id' => 3
+                    'minimum_age_id' => 3,
+                    'type_id' => 15
                 ],
                 [
-                    'name' => "Basic Musical Instruments",
+                    'name' => "Basic Musical Instruments for beginners",
                     'description' => "Guitars, keyboards, or electric drums for beginners.",
                     'image' => "https://res.cloudinary.com/dq2kswexq/image/upload/v1734336150/ChristmasToyFactory/BasicMusicalInstruments.jpg",
-                    'minimum_age_id' => 3
+                    'minimum_age_id' => 3,
+                    'type_id' => 16
                 ],
                 [
-                    'name' => "Sports Games",
+                    'name' => "Basketball Set: Ball and Hoop Combo",
                     'description' => "Balls, rackets, or accessories for specific sports.",
                     'image' => "https://res.cloudinary.com/dq2kswexq/image/upload/v1734336150/ChristmasToyFactory/SportsGames.jpg",
-                    'minimum_age_id' => 3
+                    'minimum_age_id' => 3,
+                    'type_id' => 12
                 ],
                 [
-                    'name' => "Youth Books",
-                    'description' => "Series like Harry Potter, Percy Jackson, or graphic novels.",
+                    'name' => "Complete Harry Potter Book Series",
+                    'description' => "All the complete books of the wonderful Harry Potter saga.",
                     'image' => "https://res.cloudinary.com/dq2kswexq/image/upload/v1734336150/ChristmasToyFactory/YouthBooks.jpg",
-                    'minimum_age_id' => 3
+                    'minimum_age_id' => 3,
+                    'type_id' => 17
                 ]
             ];
 
