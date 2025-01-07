d1781bed4ca99103b9e5e84c67ce6faafe591be5
Mangel111111111 <mangel111111111@gmail.com>
2025-01-07 15:59:10 +0100

    feat: add kits counter

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Kid;
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $totalKids = Kid::count();
+        $goodKids = Kid::where('attitude', true)->count();
+        $badKids = Kid::where('attitude', false)->count();
+        $kids = Kid::all();
+
+        return view('home', compact('totalKids', 'goodKids', 'badKids', 'kids'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Kid $kid)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Kid $kid)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Kid $kid)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Kid $kid)
+    {
+        //
+    }
+}
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,5 @@
 @extends('layouts.index')
+
 @section('content')
 <section class="flex items-center justify-center h-[28.8rem]">
     <div class="text-center">
@@ -9,23 +10,31 @@
             Here, in the Magic Workshop, they create the toys
             that will make millions of children around the world smile.
         </p>
-    <div class="flex flex-wrap items-center justify-center gap-24 pt-7 pb-8 h-full ">
-        <a href="{{ route('santa') }}"> <button
-            class="flex justify-center bg-[#ffffff80] p-6 rounded-lg bg-center bg-cover transform hover:scale-110 transition-transform duration-500 h-[17rem] w-[18rem]">
-            <div class="font-bold text-3xl">
-                <img class="h-[13rem] w-[13rem]" src="img/GitanoSupremo.png" alt="" />
-            </div>
-            </button>
-        </a>
-        <a href="{{ route('elf') }}"> <button
-            class="flex justify-center bg-[#ffffff80] p-6 rounded-lg bg-center bg-cover transform hover:scale-110 transition-transform duration-500 h-[17rem] w-[18rem]">
 
-            <div class="font-bold text-3xl">
-                <img class="pl-3 h-[13rem] w-[13rem]" src="img/Gitanillo.png" alt="" />
-            </div>
+        <!-- Contador de niños buenos y malos -->
+        <div class="mt-6 text-white">
+            <p class="text-2xl font-semibold">📊 Kids Summary:</p>
+            <p class="text-lg">👶 Total Kids: <span class="font-bold">{{ $totalKids }}</span></p>
+            <p class="text-lg">😊 Good Kids: <span class="font-bold">{{ $goodKids }}</span></p>
+            <p class="text-lg">😈 Bad Kids: <span class="font-bold">{{ $badKids }}</span></p>
+        </div>
 
-            </button>
-        </a>
+        <div class="flex flex-wrap items-center justify-center gap-24 pt-7 pb-8 h-full ">
+            <a href="{{ route('santa') }}">
+                <button class="flex justify-center bg-[#ffffff80] p-6 rounded-lg bg-center bg-cover transform hover:scale-110 transition-transform duration-500 h-[17rem] w-[18rem]">
+                    <div class="font-bold text-3xl">
+                        <img class="h-[13rem] w-[13rem]" src="img/GitanoSupremo.png" alt="" />
+                    </div>
+                </button>
+            </a>
+            <a href="{{ route('elf') }}">
+                <button class="flex justify-center bg-[#ffffff80] p-6 rounded-lg bg-center bg-cover transform hover:scale-110 transition-transform duration-500 h-[17rem] w-[18rem]">
+                    <div class="font-bold text-3xl">
+                        <img class="pl-3 h-[13rem] w-[13rem]" src="img/Gitanillo.png" alt="" />
+                    </div>
+                </button>
+            </a>
+        </div>
     </div>
 </section>
 @endsection
