40e22c8a9e41c223473db84db5d249e8a1b3d861
Antonio Guillén García <119064532+AntonioGuillen123@users.noreply.github.com>
2025-01-08 14:17:38 +0100

    feat: Add relations to gender and country on kid model

--- app/Models/Kid.php
+++ app/Models/Kid.php
@@ -3,6 +3,8 @@
 namespace App\Models;
 
 use App\Models\KidToy;
+use App\Models\Gender;
+use App\Models\Country;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Testing\Fluent\Concerns\Has;
@@ -20,6 +22,16 @@ class Kid extends Model
         'age',
         'attitude'
     ];
+
+    public function gender()
+    {
+        return $this->belongsTo(Gender::class);
+    }
+
+    public function country()
+    {
+        return $this->belongsTo(Country::class);
+    }
 
     public function toys()
     {
