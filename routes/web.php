64ce03c720fc3037a2659503c63d2bd2e845b7e8
Antonio Guillén García <119064532+AntonioGuillen123@users.noreply.github.com>
2025-01-08 12:14:59 +0100

    feat: Add listOfGifts on web

--- routes/web.php
+++ routes/web.php
@@ -11,3 +11,5 @@ Route::get('/santa', [KidController::class, 'index'])->name('santa');
 Route::get('/santa/{id}', [KidController::class, 'show'])->name('santaShow');
 Route::get('/elf', [ToyController::class, 'index'])->name('elf');
 Route::get('/elf/{id}', [ToyController::class, 'show'])->name('elfShow');
+
+Route::get('/santa/createListOfGifts', [KidController::class, 'listOfGifts'])->name('santaGifts');
