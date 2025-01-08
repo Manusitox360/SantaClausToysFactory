d40328fad8d2d4d1a93ef7a00d9c0fd4d9922c9d
Mangel111111111 <mangel111111111@gmail.com>
2025-01-08 11:08:53 +0100

    fix: HomeController route

---
+++ resources/views/components/kidCount.blade.php
@@ -0,0 +1,6 @@
+<div class="mt-6 text-white">
+    <p class="text-2xl font-semibold">📊 Kids Summary:</p>
+    <p class="text-lg">👶 Total Kids: <span class="font-bold">{{ $totalKids }}</span></p>
+    <p class="text-lg">😊 Good Kids: <span class="font-bold">{{ $goodKids }}</span></p>
+    <p class="text-lg">😈 Bad Kids: <span class="font-bold">{{ $badKids }}</span></p>
+</div>
