3628c52d523d1fb922a8788af023aff1fe5fee98
Mangel111111111 <mangel111111111@gmail.com>
2025-01-09 09:08:57 +0100

    fix: add mising line from Kid and database seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,7 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
-use KidSeeder;
+use Database\Seeders\KidSeeder;
 use App\Models\Kid;
 use Database\Seeders\ToySeeder;
 use Illuminate\Database\Seeder;
