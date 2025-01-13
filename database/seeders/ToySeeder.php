095c49d180c05bb6cb84e90409b0d0ba95491f34
Antonio Guillén García <119064532+AntonioGuillen123@users.noreply.github.com>
2025-01-13 20:00:36 +0100

    fix: Changes ages range for toy seeders

--- database/seeders/ToySeeder.php
+++ database/seeders/ToySeeder.php
@@ -14,7 +14,7 @@ class ToySeeder extends Seeder
     public function run(): void
     {
         $toys = [
-                // Toys for ages 0 and up
+                // Toys for ages range 0 - 3
                 [
                     'name' => "Colorful Baby Rattle",
                     'description' => "Colorful rattle that stimulates the baby's senses.",
@@ -37,27 +37,6 @@ class ToySeeder extends Seeder
                     'toy_type_id' => 1
                 ],
                 [
-                    'name' => "Baby Books Set",
-                    'description' => "Books for safe handling and exploration.",
-                    'image' => "https://res.cloudinary.com/dq2kswexq/image/upload/v1734336150/ChristmasToyFactory/PlasticBooks.jpg",
-                    'minimum_age_id' => 1,
-                    'toy_type_id' => 3
-                ],
-                [
-                    'name' => "Large Stackable Blocks",
-                    'description' => "Soft blocks to encourage motor skills.",
-                    'image' => "https://res.cloudinary.com/dq2kswexq/image/upload/v1734336150/ChristmasToyFactory/LargeStackableBlocks.jpg",
-                    'minimum_age_id' => 1,
-                    'toy_type_id' => 4
-                ],
-                [
-                    'name' => "Interactive Play Mats",
-                    'description' => "Mats with lights, sounds, and varied textures.",
-                    'image' => "https://res.cloudinary.com/dq2kswexq/image/upload/v1734336150/ChristmasToyFactory/InteractivePlayMats.jpg",
-                    'minimum_age_id' => 1,
-                    'toy_type_id' => 5
-                ],
-                [
                     'name' => "Silicone Teething Ring for Babies",
                     'description' => "Soft and safe teething toy to soothe gums.",
                     'image' => "https://res.cloudinary.com/dq2kswexq/image/upload/v1734336150/ChristmasToyFactory/TeethingToy.jpg",
@@ -65,105 +44,130 @@ class ToySeeder extends Seeder
                     'toy_type_id' => 6
                 ],
 
-                // Toys for ages 7 and up
+                // Toys for ages range 3 - 7
+                [
+                    'name' => "Young Kids Books Set",
+                    'description' => "Books for safe handling and exploration.",
+                    'image' => "https://res.cloudinary.com/dq2kswexq/image/upload/v1734336150/ChristmasToyFactory/PlasticBooks.jpg",
+                    'minimum_age_id' => 2,
+                    'toy_type_id' => 3
+                ],
+                [
+                    'name' => "Large Stackable Blocks",
+                    'description' => "Soft blocks to encourage motor skills.",
+                    'image' => "https://res.cloudinary.com/dq2kswexq/image/upload/v1734336150/ChristmasToyFactory/LargeStackableBlocks.jpg",
+                    'minimum_age_id' => 2,
+                    'toy_type_id' => 4
+                ],
+                [
+                    'name' => "Interactive Play Mats",
+                    'description' => "Mats with lights, sounds, and varied textures.",
+                    'image' => "https://res.cloudinary.com/dq2kswexq/image/upload/v1734336150/ChristmasToyFactory/InteractivePlayMats.jpg",
+                    'minimum_age_id' => 2,
+                    'toy_type_id' => 5
+                ],
+
+                // Toys for ages range 7 - 12
                 [
                     'name' => "Advanced LEGO Star Wars Ship",
                     'description' => "LEGO set with challenging models.",
                     'image' => "https://res.cloudinary.com/dq2kswexq/image/upload/v1734336150/ChristmasToyFactory/AdvancedLego.jpg",
-                    'minimum_age_id' => 2,
+                    'minimum_age_id' => 3,
                     'toy_type_id' => 7
                 ],
                 [
                     'name' => "Educational Go Genius in English",
                     'description' => "Monopoly Junior for learning through play.",
                     'image' => "https://res.cloudinary.com/dq2kswexq/image/upload/v1734336150/ChristmasToyFactory/EducationalBoardGame.jpg",
-                    'minimum_age_id' => 2,
+                    'minimum_age_id' => 3,
                     'toy_type_id' => 8
                 ],
                 [
                     'name' => "Idler Gear Experiment Kit",
                     'description' => "Kit for exploring science with fun experiments.",
                     'image' => "https://res.cloudinary.com/dq2kswexq/image/upload/v1734336150/ChristmasToyFactory/ExperimentKit.jpg",
-                    'minimum_age_id' => 2,
+                    'minimum_age_id' => 3,
                     'toy_type_id' => 9
                 ],
+
+                // Toys for ages range 12 - 16
                 [
                     'name' => "Ocean Adventure Puzzle (300 pieces)",
                     'description' => "Puzzles that stimulate logic and concentration.",
                     'image' => "https://res.cloudinary.com/dq2kswexq/image/upload/v1734336150/ChristmasToyFactory/Puzzles.jpg",
-                    'minimum_age_id' => 2,
+                    'minimum_age_id' => 4,
                     'toy_type_id' => 10
                 ],
                 [
                     'name' => "Creative Art Set: Watercolors, Clay & More",
                     'description' => "Kits with watercolors, clay, and creative crafts.",
                     'image' => "https://res.cloudinary.com/dq2kswexq/image/upload/v1734336150/ChristmasToyFactory/CraftMaterials.jpg",
-                    'minimum_age_id' => 2,
+                    'minimum_age_id' => 4,
                     'toy_type_id' => 11
                 ],
                 [
                     'name' => "Kids' Blue Mountain Bike (20-inch wheels)",
                     'description' => "Vehicle to develop balance and physical activity.",
                     'image' => "https://res.cloudinary.com/dq2kswexq/image/upload/v1734336150/ChristmasToyFactory/Bicycle.jpg",
-                    'minimum_age_id' => 2,
+                    'minimum_age_id' => 4,
                     'toy_type_id' => 12
                 ],
                 [
                     'name' => "Mini Programmable Robot for Kids",
                     'description' => "Simple programmable robots or small drones.",
                     'image' => "https://res.cloudinary.com/dq2kswexq/image/upload/v1734336150/ChristmasToyFactory/BasicTechToys.jpg",
-                    'minimum_age_id' => 2,
+                    'minimum_age_id' => 4,
                     'toy_type_id' => 13
                 ],
 
-                // Toys for ages 12 and up
+                // Toys for ages range 16 - 18
                 [
                     'name' => "High-Tech Drone with HD Camera",
                     'description' => "Advanced drone with remote control and integrated camera.",
                     'image' => "https://res.cloudinary.com/dq2kswexq/image/upload/v1734336150/ChristmasToyFactory/CameraDrone.jpg",
-                    'minimum_age_id' => 3,
+                    'minimum_age_id' => 5,
                     'toy_type_id' => 13
                 ],
                 [
                     'name' => "Risk: The Strategy Board Game",
                     'description' => "Board game like Risk to develop strategic skills.",
                     'image' => "https://res.cloudinary.com/dq2kswexq/image/upload/v1734336150/ChristmasToyFactory/StrategyGame.jpg",
-                    'minimum_age_id' => 3,
+                    'minimum_age_id' => 5,
                     'toy_type_id' => 8
                 ],
                 [
                     'name' => "Arduino Starter Kit for Robotics Projects",
                     'description' => "Arduino kit to learn robotics and programming.",
                     'image' => "https://res.cloudinary.com/dq2kswexq/image/upload/v1734336150/ChristmasToyFactory/RoboticsKit.jpg",
-                    'minimum_age_id' => 3,
+                    'minimum_age_id' => 5,
                     'toy_type_id' => 14
                 ],
                 [
                     'name' => "The Legend of Yuan: The crazy man on the road",
                     'description' => "Adventure or strategy titles suitable for teenagers.",
                     'image' => "https://res.cloudinary.com/dq2kswexq/image/upload/v1734336150/ChristmasToyFactory/VideoGames.jpg",
-                    'minimum_age_id' => 3,
+                    'minimum_age_id' => 5,
                     'toy_type_id' => 15
                 ],
                 [
                     'name' => "Basic Musical Instruments for beginners",
                     'description' => "Guitars, keyboards, or electric drums for beginners.",
                     'image' => "https://res.cloudinary.com/dq2kswexq/image/upload/v1734336150/ChristmasToyFactory/BasicMusicalInstruments.jpg",
-                    'minimum_age_id' => 3,
+                    'minimum_age_id' => 5,
                     'toy_type_id' => 16
                 ],
                 [
                     'name' => "Basketball Set: Ball and Hoop Combo",
                     'description' => "Balls, rackets, or accessories for specific sports.",
                     'image' => "https://res.cloudinary.com/dq2kswexq/image/upload/v1734336150/ChristmasToyFactory/SportsGames.jpg",
-                    'minimum_age_id' => 3,
+                    'minimum_age_id' => 5,
                     'toy_type_id' => 12
                 ],
                 [
                     'name' => "Complete Harry Potter Book Series",
                     'description' => "All the complete books of the wonderful Harry Potter saga.",
                     'image' => "https://res.cloudinary.com/dq2kswexq/image/upload/v1734336150/ChristmasToyFactory/YouthBooks.jpg",
-                    'minimum_age_id' => 3,
+                    'minimum_age_id' => 5,
                     'toy_type_id' => 17
                 ],
 
