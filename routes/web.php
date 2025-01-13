f6eb9e2b8ebdabbbb6d03e2a75659e4bae084363
Kalixto73 <Kalixto75@gmail.com>
2025-01-13 11:40:06 +0100

    merge/Merge ShowToyBlade and SantaShowBlade

--- routes/web.php
+++ routes/web.php
@@ -11,3 +11,7 @@ Route::get('/santa', [KidController::class, 'index'])->name('santa');
 Route::get('/santa/{id}', [KidController::class, 'show'])->name('santaShow');
 Route::get('/elf', [ToyController::class, 'index'])->name('elf');
 Route::get('/elf/{id}', [ToyController::class, 'show'])->name('elfShow');
+
+Route::get('/gift', function () {
+    return view('gift');
+})->name('gift');
