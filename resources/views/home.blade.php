6a4d70d639f46c47bc1ee17cd5a0d80fe794a570
Kalixto73a <Kalixto75@gmail.com>
2025-01-17 19:15:57 +0100

    feat:responsiveForMobile

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,18 +1,18 @@
 @extends('layouts.index')
 @section('content')
-<section class="flex flex-col items-center justify-center overflow-auto">
+<section class="flex flex-col items-center justify-center overflow-auto xl:h-[829px]">
     <div class="flex justify-center items-center flex-col text-center">
-        <h1 class="text-4xl font-bold text-white p-2">🎅 Welcome to Santa Claus Toys Factory! 🎁</h1>
+        <h1 class="text-4xl font-bold text-white p-2 z-10">🎅 Welcome to Santa Claus Toys Factory! 🎁</h1>
         <p class="text-lg text-white">
             From the heart of the North Pole, Father Christmas and his faithful elves work tirelessly to make this Christmas an unforgettable moment. 
             <br>
             Here, in the Magic Workshop, they create the toys
             that will make millions of children around the world smile.
         </p>
-        <div class="flex flex-wrap items-center justify-center gap-24 pt-7 pb-4 h-full ">
+        <div class="flex flex-wrap items-center justify-center gap-10 sm:gap-24 py-4 h-full ">
             <a href="{{ route('santa') }}">
                 <button class="flex items-center justify-center bg-[#ffffff80] p-6 rounded-lg bg-center bg-cover transform hover:scale-110 transition-transform duration-500 h-[19rem] w-[18rem]">
-                <div class="font-bold text-3xl">
+                <div class="items-center font-bold text-3xl">
                     <img class="h-[14rem] w-[13rem]" src="img/GitanoSupremo.png" alt="" />
                 </div>
                 </button>
@@ -25,17 +25,17 @@
                 </button>
             </a>
         </div>
-    </div>
-    <div class="flex flex-wrap space-x-[6rem] pb-[1.8rem]">
-        <div class="bg-[#ffffff80] rounded-lg">
-            <h2 class="text-xl font-semibold text-center text-white">{{ $chart->options['chart_title'] }}</h2>
-            <div class="flex justify-center items-center w-[18rem]">{!! $chart->renderHtml() !!}</div>
-            <div class="text-center text-base font-semibold text-white">Total Kids: {{$totalKids}}</div>
-        </div>
-        <div class="bg-[#ffffff80] rounded-lg">
-            <h2 class="text-xl font-semibold text-center text-white">{{ $chart2->options['chart_title'] }}</h2>
-            <div class="flex justify-center items-center w-[18rem]">{!! $chart2->renderHtml() !!}</div>
-            <div class="text-center text-base font-semibold text-white">Total Toys: {{$totalToys}} {{-- {{$ageRanges}} --}}</div>
+        <div class="flex flex-wrap items-center justify-center gap-10 sm:gap-24 py-5 h-full">
+            <div class="bg-[#ffffff80] rounded-lg">
+                <h2 class="text-xl font-semibold text-center text-white">{{ $chart->options['chart_title'] }}</h2>
+                <div class="flex justify-center items-center w-[18rem]">{!! $chart->renderHtml() !!}</div>
+                <div class="text-center text-base font-semibold text-white">Total Kids: {{$totalKids}}</div>
+            </div>
+            <div class="bg-[#ffffff80] rounded-lg">
+                <h2 class="text-xl font-semibold text-center text-white">{{ $chart2->options['chart_title'] }}</h2>
+                <div class="flex justify-center items-center w-[18rem]">{!! $chart2->renderHtml() !!}</div>
+                <div class="text-center text-base font-semibold text-white">Total Toys: {{$totalToys}} {{-- {{$ageRanges}} --}}</div>
+            </div>
         </div>
     </div>
 </section>
