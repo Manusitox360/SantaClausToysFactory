225f8f8e8db33d2f0d02592093be41c9353481bc
Kalixto73a <Kalixto75@gmail.com>
2025-01-20 12:44:53 +0100

    feat:Delete Graphic And Add Data On Card Of Home Blade

--- resources/views/elfShow.blade.php
+++ resources/views/elfShow.blade.php
@@ -32,31 +32,4 @@
         </div>
     </div>
 </div>
-{{-- <div class="px-[8rem] gap-[6rem] flex flex-wrap items-center justify-center lg:h-[461px] xl:h-[829px]">
-    <div class="m-4 w-full bg-[#ffffff80] border border-gray-200 rounded-lg shadow">
-        <h5 class="text-xl uppercase font-bold text-black border-b border-black p-4 flex justify-between items-center">
-            <button class="ml-14 transition hover:scale-110 duration-500 uppercase font-bold bg-candy-cane rounded-lg px-4 py-2">
-                <a href="{{route('elf')}}">Back To Toy List</a>
-            </button>
-            <span class="ml-[4.3rem] text-center flex-1">{{$toy->name}}</span>
-        </h5>
-        <div class="flex flex-row justify-between items-center p-4 pt-[1rem]">
-            <img 
-                class="w-[18rem] h-[16.9rem] m-4 rounded-lg shadow-lg" 
-                src="{{$toy->image}}" 
-                alt="Bonnie image"
-            />
-            <div class="flex flex-col justify-center items-center flex-grow">
-                <div class="flex gap-[4rem] flex-col items-center pt-4 ">
-                    <span class="text-center text-xl font-medium text-black">
-                        Description: <br>{{$toy->description}}
-                    </span>
-                    <span class="text-center text-xl font-medium text-black">
-                        Age: <br>{{$toy->minimumAge}}
-                    </span>
-                </div>
-            </div>
-        </div>
-    </div>
-</div> --}}
 @endsection
