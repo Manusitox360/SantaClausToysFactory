6d1221f4c9cae2177e4845917117d11254bb3447
Kalixto73a <Kalixto75@gmail.com>
2025-01-20 08:59:25 +0100

    feat:resposiveForTablet

--- resources/views/elfShow.blade.php
+++ resources/views/elfShow.blade.php
@@ -1,19 +1,19 @@
 @extends('layouts.index')
 @section('content')
 <div class="flex items-center justify-center pt-[1rem]">
-    <button class="block sm:hidden transition hover:scale-110 duration-500 uppercase font-bold bg-candy-cane rounded-lg px-4 py-2">
+    <button class="block lg:hidden transition hover:scale-110 duration-500 uppercase font-bold bg-candy-cane rounded-lg px-4 py-2">
         <a href="{{route('elf')}}">Back To Toy List</a>
     </button>
 </div>
-<div class="px-[2rem] py-[1rem] sm:py-0 sm:px-[8rem] flex flex-wrap items-center justify-center lg:h-[445px] xl:h-[813px]">
+<div class="px-[2rem] py-[1rem] lg:py-0 lg:px-[8rem] flex flex-wrap items-center justify-center h-[613px] sm:h-[686px] lg:h-[726px] xl:h-[813px]">
     <div class="w-full bg-[#ffffff80] border border-gray-200 rounded-lg shadow">
         <h5 class="text-xl uppercase font-bold text-black border-b border-black p-4 flex justify-between items-center">
-            <button class="hidden sm:block ml-14 transition hover:scale-110 duration-500 uppercase font-bold bg-candy-cane rounded-lg px-4 py-2">
+            <button class="hidden lg:block ml-14 transition hover:scale-110 duration-500 uppercase font-bold bg-candy-cane rounded-lg px-4 py-2">
                 <a href="{{route('elf')}}">Back To Toy List</a>
             </button>
-            <span class="sm:ml-14 text-center flex-1">{{$toy->name}}</span>
+            <span class="lg:ml-14 text-center flex-1">{{$toy->name}}</span>
         </h5>
-        <div class="flex flex-col sm:flex-row justify-between items-center p-4 pt-[1rem]">
+        <div class="flex flex-col lg:flex-row justify-between items-center p-4 pt-[1rem]">
             <img 
                 class="w-[18rem] h-[16.9rem] m-4 rounded-lg shadow-lg" 
                 src="{{$toy->image}}" 
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.index')
 @section('content')
-<section class="flex flex-col items-center justify-center overflow-auto xl:h-[829px]">
+<section class="flex flex-col items-center justify-center overflow-auto md:h-[920px] xl:h-[829px]">
     <div class="flex justify-center items-center flex-col text-center">
         <h1 class="text-4xl font-bold text-white p-2 z-10">🎅 Welcome to Santa Claus Toys Factory! 🎁</h1>
         <p class="text-lg text-white">
@@ -9,7 +9,7 @@
             Here, in the Magic Workshop, they create the toys
             that will make millions of children around the world smile.
         </p>
-        <div class="flex flex-wrap items-center justify-center gap-10 sm:gap-24 py-4 h-full ">
+        <div class="flex flex-wrap items-center justify-center gap-10 sm:gap-12 md:gap-24 py-4 h-full ">
             <a href="{{ route('santa') }}">
                 <button class="flex items-center justify-center bg-[#ffffff80] p-6 rounded-lg bg-center bg-cover transform hover:scale-110 transition-transform duration-500 h-[19rem] w-[18rem]">
                 <div class="items-center font-bold text-3xl">
@@ -25,7 +25,7 @@
                 </button>
             </a>
         </div>
-        <div class="flex flex-wrap items-center justify-center gap-10 sm:gap-24 py-5 h-full">
+        <div class="flex flex-wrap items-center justify-center gap-10 sm:gap-12 md:gap-24 py-5 h-full">
             <div class="bg-[#ffffff80] rounded-lg">
                 <h2 class="text-xl font-semibold text-center text-white">{{ $chart->options['chart_title'] }}</h2>
                 <div class="flex justify-center items-center w-[18rem]">{!! $chart->renderHtml() !!}</div>
